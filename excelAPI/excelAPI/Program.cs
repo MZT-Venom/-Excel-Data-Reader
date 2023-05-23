8b9f3482f8fd5524d9ef255c51dbb1a36f498b9e
MZT-Venom <81874355+MZT-Venom@users.noreply.github.com>
2023-05-23 07:45:33 -0700

    Whole project
    The Excel Data Reader is a command line interface (CLI) tool built in C# that enables users to read data from Excel files. With this tool, you can easily extract and display data from Excel worksheets directly from the command line.

---
+++ excelAPI/excelAPI/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace excelAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"new.xlsx";
+
+            // Open the Excel file
+            using (SpreadsheetDocument spreadsheetDocument = SpreadsheetDocument.Open(filePath, false))
+            {
+                WorkbookPart workbookPart = spreadsheetDocument.WorkbookPart;
+                WorksheetPart worksheetPart = workbookPart.WorksheetParts.First();
+                SheetData sheetData = worksheetPart.Worksheet.Elements<SheetData>().First();
+             
+                // Loop through the rows and columns of the worksheet
+                foreach (Row row in sheetData.Elements<Row>())
+                {
+                    foreach (Cell cell in row.Elements<Cell>())
+                    {
+                        string value = GetCellValue(cell, workbookPart);
+                        Console.Write("{0,-20}", value);
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadKey();
+        }
+        private static string GetCellValue(Cell cell, WorkbookPart workbookPart)
+        {
+            string value = cell.InnerText;
+
+            if (cell.DataType != null && cell.DataType.Value == CellValues.SharedString)
+            {
+                int ssid = int.Parse(value);
+                SharedStringItem ssi = workbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(ssid);
+                value = ssi.InnerText;
+            }
+
+            return value;
+        }
+    }
+}
